9298c7d275d97252ff450c32cdcc980cc4c6a72d
gobalkrishnan <gobalkrishnan.books@gmail.com>
2024-12-30 17:56:28 +0530

    file structure and tile map

---
+++ public/js/code_51_75.js
@@ -0,0 +1,32 @@
+class Code_51_75{
+    static c51 = ``;
+    static c52 = ``;
+    static c53 = ``;
+    static c54 = ``;
+    static c55 = ``;
+    
+    static c56 = ``;
+    static c57 = ``;
+    static c58 = ``;
+    static c59 = ``;
+    static c60 = ``;
+    
+    static c61 = ``;
+    static c62 = ``;
+    static c63 = ``;
+    static c64 = ``;
+    static c65 = ``;
+    
+    static c66 = ``;
+    static c67 = ``;
+    static c68 = ``;
+    static c69 = ``;
+    static c70 = ``;
+    
+    static c71 = ``;
+    static c72 = ``;
+    static c73 = ``;
+    static c74 = ``;
+    static c75 = ``;
+    
+}
