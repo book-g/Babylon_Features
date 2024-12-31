e9e9b2c7221f055ddd1ecf3c8aed21ae632e54c2
gobalkrishnan <gobalkrishnan.books@gmail.com>
2024-12-31 16:42:59 +0530

    noe

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const PORT = 3000;
 // Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use('/3dmodel', express.static('3dmodel', {
+    setHeaders: (res, path, stat) => {
+        if (path.endsWith('.glb')) {
+            res.set('Content-Type', 'model/gltf-binary');
+        }
+    }
+}));
+
+
 // Serve the main HTML file
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
